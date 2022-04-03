b4a02e8f69131258c15ee4a2739ec7ebe7101e6e
Joey Bruno <brunojoey12@gmail.com>
2022-04-03 13:27:03 -0500

    Heroku Create initilization

--- thrive-therapy/src/App.js
+++ thrive-therapy/src/App.js
@@ -3,15 +3,15 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Banner from "./components/Banner";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Questions from "./pages/Questions";
-import Resources from "./pages/Resources";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Banner from "./components/Banner";
+// import Home from "./pages/Home";
+// import Services from "./pages/Services";
+// import Contact from "./pages/Contact";
+// import About from "./pages/About";
+// import Questions from "./pages/Questions";
+// import Resources from "./pages/Resources";
 import ComingSoon from "./pages/ComingSoon";
 import NotFound from "./pages/NotFound"
 import './App.scss';
@@ -23,7 +23,7 @@ function App() {
         {/* <Banner />
         <Header /> */}
         <Routes className="content">
-          {/* <Route path="/" element={<Home />}/>
+          {/* <Route path="/home" element={<Home />}/>
           <Route path="/services" element={<Services />}/>
           <Route path="/contact" element={<Contact />}/>
           <Route path="/about" element={<About />}/>
