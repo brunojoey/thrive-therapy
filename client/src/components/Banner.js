ea1451fa8a79739bbc1a2f331701f3ab8ce8667b
Joey Bruno <brunojoey12@gmail.com>
2022-04-08 11:16:12 -0500

    Migrated old project files to new Client folder

---
+++ client/src/components/Banner.js
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h3 className="banner-content">PAYMENT</h3>
+    </div>
+  )
+}
+
+export default Banner;
---
+++ client/src/pages/Resources.js
@@ -0,0 +1,9 @@
+const Resources = () => {
+  return (
+    <div className="content">
+      <h1>Resources</h1>
+    </div>
+  )
+}
+
+export default Resources;
