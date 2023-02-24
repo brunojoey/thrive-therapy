b93fe84dae27c5198c990a8045ef28ba06435455
Joey Bruno <brunojoey12@gmail.com>
2023-02-24 11:53:33 -0600

    Added some borders to the icons within the Contact Page and added a Facebook link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import "./index.scss";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <section className="footer-info">
-        <h2>Thrive Pediatric Therapy</h2>
+        <h1>Thrive Pediatric Therapy</h1>
         <p>170 W Franklin Street Gallatin, TN 37066 </p>
         <p>(615) 802-8051</p>
         <p>Monday - Thursday 8-6 PM</p>
@@ -14,7 +14,9 @@ const Footer = () => {
       </section>
       <section className="social-links">
         <FontAwesomeIcon icon={faInstagram} className="instagram" />
-        <a href="https://instagram.com/thrive.pediatrictherapy/" className="instagram-link">@thrive.pediatrictherapy</a>
+        <a href="https://instagram.com/thrive.pediatrictherapy/" className="instagram-link">Our Instagram</a>
+        <FontAwesomeIcon icon={faFacebook} className="facebook" />
+        <a href="https://www.facebook.com/thrivepediatrictherapytn" className="facebook-link">Our Facebook</a>
       </section>
     </footer>
   )
