45e1bcb7afd27505e15ccf69306b115b2f959650
Joey Bruno <brunojoey12@gmail.com>
2023-07-13 14:17:35 -0500

    Added Katrina Reed and Sydnie Dudek to the About page with styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,9 @@ import kayleePhoto from "../assets/owner-files/2022-11-13_01-34-31_030.jpeg"
 import arianaPhoto from "../assets/owner-files/IMG_2299.jpg";
 import aliciaPhoto from "../assets/owner-files/AliciaBioPhoto.jpg";
 import maggieWebbPhoto from "../assets/owner-files/421E801A-6E2B-404D-9284-C7370F534D92_1_105_c.jpeg";
-import brittanyPhoto from "../assets/owner-files/image0.jpeg"
+import brittanyPhoto from "../assets/owner-files/image0.jpeg";
+import sydniePhoto from "../assets/owner-files/IMG_9570.heic";
+import katrinaPhoto from "../assets/owner-files/IMG_0961.jpg";
 import drawing from "../assets/stock-photos/erika-fletcher-YfNWGrQI3a4-unsplash.jpg"
 
 const About = () => {
@@ -121,28 +123,22 @@ const About = () => {
         <section className="about-staff">
           <div className="about-staff-otr">
             <h3 style={{ textAlign: "center" }}>Meet Our Occuptational Therapists!</h3>
-            <div className="about-staff-jordan">
+            <div className="about-staff-alicia">
               <div>
-                <h3>Occupational Therapist</h3>
-                <img src={jordanPhoto} alt="Jordan Sanders" width="250" />
+                <h3>Certified Occupational Therapist Assistant/Licensed</h3>
+                <img src={aliciaPhoto} alt="Alicia Kurth" width="250" />
               </div>
               <div>
-                <h2>Jordan Sanders | OTR/L</h2>
+                <h2>Alicia Kurth | COTA/L</h2>
                 <p>
-                  Jordan graduated from the University of Tennessee Health Science
-                  Center in 2016 with her Masters in Occupational Therapy. Her
-                  Master's degree was preceded by a Bachelor's degree in Psychology
-                  from Cumberland University. She has experience working in
-                  outpatient pediatrics and in acute care settings. Jordan has a
-                  passion for working with children of all abilities and fostering
-                  development in the early years of life. She loves partnering with
-                  children, their families, and their team to promote meaningful
-                  participation in their activities of daily life. Jordan enjoys
-                  incorporating a combination of play-based, holistic,
-                  evidenced-based, and sensory-based approaches to treatment,
-                  encouraging children to develop functional skills in everyday
-                  activities. In her free time she enjoys traveling, trying new
-                  restaurants, spending time with family, friends, and her dog Ace.
+                  Alicia grew up in Wilson County and then completed her Associate's Degree in
+                  Occupational Therapy Assistant at Nashville State Community College in 2018. She
+                  has experience working in outpatient pediatrics. Her passion is to provide an
+                  environment for children to reach their full potential and to provide families
+                  with opportunities to foster growth within all aspects of the child's life.
+                  Alicia loves seeing the joy on a child's face when they achieve a goal they
+                  have been working toward. Outside of work, Alicia enjoys teaching dance, crafting,
+                  visiting her godchildren, and spending time with friends, family, and her dog Caya.
                 </p>
               </div>
             </div>
@@ -168,22 +164,28 @@ const About = () => {
                 <img src={arianaPhoto} alt="Ariana Musolino" width="250" />
               </div>
             </div>
-            <div className="about-staff-alicia">
+            <div className="about-staff-jordan">
               <div>
-                <h3>Certified Occupational Therapist Assistant/Licensed</h3>
-                <img src={aliciaPhoto} alt="Alicia Kurth" width="250" />
+                <h3>Occupational Therapist</h3>
+                <img src={jordanPhoto} alt="Jordan Sanders" width="250" />
               </div>
               <div>
-                <h2>Alicia Kurth | COTA/L</h2>
+                <h2>Jordan Sanders | OTR/L</h2>
                 <p>
-                  Alicia grew up in Wilson County and then completed her Associate's Degree in
-                  Occupational Therapy Assistant at Nashville State Community College in 2018. She
-                  has experience working in outpatient pediatrics. Her passion is to provide an
-                  environment for children to reach their full potential and to provide families
-                  with opportunities to foster growth within all aspects of the child's life.
-                  Alicia loves seeing the joy on a child's face when they achieve a goal they
-                  have been working toward. Outside of work, Alicia enjoys teaching dance, crafting,
-                  visiting her godchildren, and spending time with friends, family, and her dog Caya.
+                  Jordan graduated from the University of Tennessee Health Science
+                  Center in 2016 with her Masters in Occupational Therapy. Her
+                  Master's degree was preceded by a Bachelor's degree in Psychology
+                  from Cumberland University. She has experience working in
+                  outpatient pediatrics and in acute care settings. Jordan has a
+                  passion for working with children of all abilities and fostering
+                  development in the early years of life. She loves partnering with
+                  children, their families, and their team to promote meaningful
+                  participation in their activities of daily life. Jordan enjoys
+                  incorporating a combination of play-based, holistic,
+                  evidenced-based, and sensory-based approaches to treatment,
+                  encouraging children to develop functional skills in everyday
+                  activities. In her free time she enjoys traveling, trying new
+                  restaurants, spending time with family, friends, and her dog Ace.
                 </p>
               </div>
             </div>
@@ -218,7 +220,7 @@ const About = () => {
                 <img src={maggiePhoto} alt="Maggie Bill" width="250" />
               </div>
               <div>
-                <h2>Maggie Bill | M.S. CCC-SLP </h2>
+                <h2>Maggie Bill | M.S. CCC-SLP</h2>
                 <p>
                   Maggie grew up in Indiana, but has loved living in Tennessee for the
                   last several years. She graduated from Saint Louis University with her
@@ -238,7 +240,7 @@ const About = () => {
             </div>
             <div className="about-staff-kaylee">
               <div>
-                <h2>Kaylee Coghill | M.A. CCC-SLP </h2>
+                <h2>Kaylee Coghill | M.A. CCC-SLP</h2>
                 <p>
                   Kaylee feels it is a joy and privilege to help children and their
                   families learn and grow together. Kaylee uses evidence-based treatment
@@ -258,6 +260,48 @@ const About = () => {
               <div>
                 <h3 style={{ margin: ".25rem" }}>Speech Language Pathologist</h3>
                 <img src={kayleePhoto} alt="Kaylee Coghill" width="250" />
+              </div>
+            </div>
+            <div className="about-staff-sydnie">
+              <div>
+                <h3>Speech Language Pathologist</h3>
+                <img src={sydniePhoto} alt="Sydnie Dudek" width="250" />
+              </div>
+              <div>
+                <h2>Sydnie Dudek | CF-SLP</h2>
+                <p>
+                  Sydnie grew up in Ohio but has recently moved to Tennessee following 
+                  the completion of her Bachelor's degree at East Carolina University 
+                  and her Master's degree in Speech-Language Pathology at the Medical 
+                  University of South Carolina. She has experience in a variety of settings 
+                  from adult to pediatric and enjoys working with children with feeding/swallowing 
+                  difficulties, augmentative and alternative communication (AAC), autism, and 
+                  receptive and expressive language impairments. In her free time, she enjoys 
+                  exploring new areas of Nashville, cheering on the Buckeyes, and spending 
+                  time with her friends and dog, Willa. Sydnie is excited to build relationships 
+                  with children and families and help them grow in order to reach their goals!
+                </p>
+              </div>
+            </div>
+            <div className="about-staff-katrina">
+              <div>
+                <h2>Katrina Reed | M.S. CCC-SLP </h2>
+                <p>
+                  Katrina has been a speech-language pathologist for over 10 years! She enjoys
+                  working with children and helping them develop their speech, language, and
+                  feeding skills in a fun, positive, and effective way; she wants to find ways
+                  to educate parents and increase their carry-over to home. She earned her
+                  undergraduate and graduate degree from Western Kentucky University. Katrina
+                  has experience in the private practice setting, elementary school setting, and
+                  long-term care and rehabilitation facilities. She has experience with early
+                  language development, articulation, apraxia, AAC, dysphagia, and oral
+                  motor/feeding therapy. Katrina enjoys gardening, reading, traveling, and 
+                  spending time with her husband and two children.
+                </p>
+              </div>
+              <div>
+                <h3 style={{ margin: ".25rem" }}>Speech Language Pathologist</h3>
+                <img src={katrinaPhoto} alt="Katrina Reed" width="250" />
               </div>
             </div>
           </div>
